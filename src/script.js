89c38a0bccc2a9909d3f8b6067b48a158fab7a7f
Johndhee <Johndheeballesteros13@gmail.com>
2021-04-20 15:55:21 -0700

    added more JS

--- src/script.js
+++ src/script.js
@@ -1,15 +1,25 @@
 const url = "http://localhost:3000/games/1"
 
 document.addEventListener('DOMContentLoaded', event => {
-    game()
+    gotGames()
 })
 
-const game = () => {
+const gotGames = () => {
     fetch(url)
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(data => gamerList(data))
 }
 
-const gamerList = () => {
-    
+const gamerList = (gamez) => {
+
+    let div = document.createElement('div')
+    div.className = 'game'
+
+    let heading = document.createElement('h2')
+    heading.innerText = gamez.name
+
+    let img = document.createElement('img')
+    img.src = gamez.image
+
+    div.append(heading, img)
 }
