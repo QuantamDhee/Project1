da1efa1ab28852f1807f7a12a58097d1e72db6e0
Johndhee <Johndheeballesteros13@gmail.com>
2021-04-20 15:33:35 -0700

    added fetch JS

--- src/script.js
+++ src/script.js
@@ -1,2 +1,15 @@
-const url = "http://localhost:3000/games"
+const url = "http://localhost:3000/games/1"
 
+document.addEventListener('DOMContentLoaded', event => {
+    game()
+})
+
+const game = () => {
+    fetch(url)
+    .then(r => r.json())
+    .then(data => console.log(data))
+}
+
+const gamerList = () => {
+    
+}
